aac2f6a6c9c22caa937cfb029ec24af44264ff1b
olavhegg <olav.heggelund@gmail.com>
2023-05-09 16:55:32 +0200

    added some code

--- src/main/java/aiss/GitLabMiner/GitLabApi.java
+++ src/main/java/aiss/GitLabMiner/GitLabApi.java
@@ -1,5 +1,21 @@
 package aiss.GitLabMiner;
 
+import java.util.List;
+
+import aiss.gitminer.model.*;
+
 public class GitLabApi {
+
+    public List<Project> getProjects(){
+        return null;
+    };
+
+    public List<Commit> getCommits(String projectId, int sinceDays){
+        return null;
+    };
+
+    public List<Issue> getIssues(String projectId, int sinceDays){
+        return null;
+    };
 
 }
--- src/main/java/aiss/GitLabMiner/GitLabClient.java
+++ src/main/java/aiss/GitLabMiner/GitLabClient.java
@@ -1,5 +1,38 @@
 package aiss.GitLabMiner;
 
-public class GitLabClient {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import aiss.gitminer.model.*;
+
+@Service
+public class GitLabClient {
+    private final RestTemplate restTemplate;
+    private final String gitMinerUrl;
+
+    @Autowired
+    public GitLabClient(RestTemplate restTemplate, @Value("${gitminer.url}") String gitMinerUrl) {
+        this.restTemplate = restTemplate;
+        this.gitMinerUrl = gitMinerUrl;
+    }
+
+    public void sendProjects(List<Project> projects) {
+        HttpEntity<List<Project>> request = new HttpEntity<>(projects);
+        restTemplate.postForObject(gitMinerUrl + "/api/projects", request, Void.class);
+    }
+
+    public void sendCommits(List<Commit> commits) {
+        HttpEntity<List<Commit>> request = new HttpEntity<>(commits);
+        restTemplate.postForObject(gitMinerUrl + "/api/commits", request, Void.class);
+    }
+
+    public void sendIssues(List<Issue> issues) {
+        HttpEntity<List<Issue>> request = new HttpEntity<>(issues);
+        restTemplate.postForObject(gitMinerUrl + "/api/issues", request, Void.class);
+    }
 }
